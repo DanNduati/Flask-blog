8ed42c71bea8f5734683b063e47afeeca33f4009
Daniel Nduati <dannduati2@gmail.com>
2021-07-27 03:27:57 +0300

    Added restricted user account route that allows the user to update their details including uploading a profile picture

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField,FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo,ValidationError
 from flaskblog.models import User
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
 	username = StringField('Username',validators=[DataRequired(),Length(min=2,max=15)])
@@ -26,3 +28,23 @@ class LoginForm(FlaskForm):
 	password = PasswordField('Password',validators=[DataRequired()])
 	remember = BooleanField('Remember me')
 	submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+	username = StringField('Username',validators=[DataRequired(),Length(min=2,max=15)])
+	email = StringField('Email',validators=[DataRequired(),Email()])
+	picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'], 'Images only!')])#profile picture field
+	submit = SubmitField('Update')
+	#only do the custom validation checks only when the user changes any of the fields
+	#custom validation from wtforms
+	def validate_username(self, username):
+		if username.data != current_user.username:
+			user = User.query.filter_by(username=username.data).all()
+			if user:
+				raise ValidationError('Username already taken please choose a different one')
+
+	def validate_email(self, email):
+		if email.data != current_user.email:
+			email = User.query.filter_by(email=email.data).all()
+			if email:
+				raise ValidationError('Email already taken please choose a different one')
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,6 +1,9 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template,url_for,flash,redirect,request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm,LoginForm
+from flaskblog.forms import RegistrationForm,LoginForm,UpdateAccountForm
 from flaskblog.models import User, Post
 from flask_login import login_user,current_user,logout_user,login_required
 #pseudo posts for views tests 
@@ -73,7 +76,40 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-@app.route("/account")
+#function to set the users profile picture
+def save_picture(form_picture):
+    #randomize the name of the image
+    random_hex = secrets.token_hex(8)
+    #get the extension using the os module
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex+f_ext
+    #dir where the image will be saved
+    #use the root path attribute of our application 
+    #profile_pics within the static folder
+    picture_path = os.path.join(app.root_path,'static/profile_pics',picture_fn)
+    #resize the image before saving
+    output_size = (125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
+
+@app.route("/account", methods=['GET','POST'])
 @login_required
 def account():
-    return render_template('account.html',title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        #update the username and email
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!','success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static',filename='profile_pics/'+current_user.image_file)
+    return render_template('account.html',title='Account',image_file=image_file,form=form)
